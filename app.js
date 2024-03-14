e6417afc1e099e499b62ccda6be2194fd19b6072
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-03-14 06:18:46 -0700

    Updating progress to remote

--- app.js
+++ app.js
@@ -64,7 +64,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    title: err.status,
+    user: req.user,
+  });
 });
 
 module.exports = app;
